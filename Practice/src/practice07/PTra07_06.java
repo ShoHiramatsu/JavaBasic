f2af197785ea45f7ac12fbce076181c7ffe2a958
平松翔 <1031kds417813@gmail.com>
2018-02-13 18:27:41 +0900

    7-6

--- Practice/src/practice07/PTra07_06.java
+++ Practice/src/practice07/PTra07_06.java
@@ -1,4 +1,5 @@
 package practice07;
+
 /*
  * PTra07_07.java
  *   作成	LIKEIT	2017
@@ -16,8 +17,19 @@ public class PTra07_06 {
 		};
 
 		// ★ 配列arrayに格納されている全ての数字の平均を出力してください
-		double sum = array[0][0] + array[0][1] + array[0][2] + array[1][0] + array[1][1] + array[1][2] + array[1][3];
-		System.out.println(sum / array.length);
+		int sum = 0;
+		int count = 0;
+
+		for (int[] i : array) {
+
+			for (int r : i) {
+				sum += r;
+				count += 1;
+
+			}
+
+		}
+		System.out.println(sum / count);
 
 	}
 }
